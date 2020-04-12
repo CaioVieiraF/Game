49f31af491a38c98507300e53ddaabd467d9360e
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-04-12 16:15:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project_test
+ sample text
+
+This Repo is mainly for a git and github course I'm having
---
+++ Scripts/main.py
@@ -0,0 +1,10 @@
+import time
+import random
+import pygame
+
+def Display(x, y):
+    return
+
+loop = True
+while loop:
+    Display(5, 7)
